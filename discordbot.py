bdb6b3cb1877fe7fa1694b7aa9d6c4d7d6c63051
dreadnought0411 <dreadnought0411@gmail.com>
2019-07-14 02:11:02 +0900

    コメントを追加

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,10 @@ from discord.ext import commands
 import os
 import traceback
 
+# command_prefixはコマンドの最初の文字として使うもの。e.g. /ping
 bot = commands.Bot(command_prefix='/')
+
+# 実行環境のDISCORD_BOT_TOKENという環境変数からbotのトークンを取得する。今はHerokuにトークンを記入してHerokuで実行してる。
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
