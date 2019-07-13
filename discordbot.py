eb9e85921468d6fcca035c9084047c7de81bc592
dreadnought0411 <dreadnought0411@gmail.com>
2019-07-14 02:12:28 +0900

    command_prefixを$に変更

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
-# command_prefixはコマンドの最初の文字として使うもの。e.g. /ping
-bot = commands.Bot(command_prefix='/')
+# command_prefixはコマンドの最初の文字として使うもの。e.g. $ping
+bot = commands.Bot(command_prefix='$')
 
 # 実行環境のDISCORD_BOT_TOKENという環境変数からbotのトークンを取得する。今はHerokuにトークンを記入してHerokuで実行してる。
 token = os.environ['DISCORD_BOT_TOKEN']
